6cbe8797a8bc9f449556b0ead9231647861c078b
Connor Fogarty <connorflynnfogarty@gmail.com>
2021-12-17 17:50:26 -0600

    Add initial UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,27 @@
+import { Navbar, Container } from "react-bootstrap";
+import { Github, GraphUpArrow } from "react-bootstrap-icons";
+import { GITHUB_REPO, ICON_WIDTH, PROJECT_NAME } from "./utils/constants";
+
 function App() {
   return (
-    <div>Hello World!</div>
+    <>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand>
+            <GraphUpArrow size={ICON_WIDTH} />
+            <span style={{ marginLeft: "10px" }}>{PROJECT_NAME}</span>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+          <Navbar.Collapse className="justify-content-end">
+            <Navbar.Text>
+              <a href={GITHUB_REPO} target="_blank" rel="noreferrer">
+                <Github size={ICON_WIDTH} />
+              </a>
+            </Navbar.Text>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
   );
 }
 
---
+++ src/utils/constants.ts
@@ -0,0 +1,4 @@
+export const PROJECT_NAME = "Motion Visualizer";
+export const GITHUB_REPO = "https://github.com/connorff/mathjs-calc-visual";
+
+export const ICON_WIDTH = 30;
